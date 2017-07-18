eb6bcad4cdc6d50589ea9fa3a5da6e06f5d5ab1a
Jeremy Shannon <jeremyeshannon@gmail.com>
2017-07-18 16:00:25 -0500

    Create contacts.cpp

---
+++ tries-contacts/contacts.cpp
@@ -0,0 +1,156 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+class Node {
+public:
+    Node() { mContent = ' '; mMarker = false; }
+    ~Node() {}
+    char content() { return mContent; }
+    void setContent(char c) { mContent = c; }
+    bool wordMarker() { return mMarker; }
+    void setWordMarker() { mMarker = true; }
+    Node* findChild(char c);
+    void appendChild(Node* child) { mChildren.push_back(child); }
+    vector<Node*> children() { return mChildren; }
+
+private:
+    char mContent;
+    bool mMarker;
+    vector<Node*> mChildren;
+};
+
+class Trie {
+public:
+    Trie();
+    ~Trie();
+    void addWord(string s);
+    int searchWord(string s);
+    void deleteWord(string s);
+private:
+    Node* root;
+};
+
+Node* Node::findChild(char c)
+{
+    for ( int i = 0; i < mChildren.size(); i++ )
+    {
+        Node* tmp = mChildren.at(i);
+        if ( tmp->content() == c )
+        {
+            return tmp;
+        }
+    }
+
+    return NULL;
+}
+
+Trie::Trie()
+{
+    root = new Node();
+}
+
+Trie::~Trie()
+{
+    // Free memory
+}
+
+void Trie::addWord(string s)
+{
+    Node* current = root;
+
+    if ( s.length() == 0 )
+    {
+        current->setWordMarker(); // an empty word
+        return;
+    }
+
+    for ( int i = 0; i < s.length(); i++ )
+    {        
+        Node* child = current->findChild(s[i]);
+        if ( child != NULL )
+        {
+            current = child;
+        }
+        else
+        {
+            Node* tmp = new Node();
+            tmp->setContent(s[i]);
+            current->appendChild(tmp);
+            current = tmp;
+        }
+        if ( i == s.length() - 1 )
+            current->setWordMarker();
+    }
+}
+
+
+int countWordsFromNode(Node* n) {
+    int total = 0;
+    if (n->wordMarker()) total += 1;
+    for (Node* n1 : n->children()) {
+        total += countWordsFromNode(n1);
+    }
+    return total;
+}
+
+int Trie::searchWord(string s)
+{
+    Node* current = root;
+
+    while ( current != NULL )
+    {
+        for ( int i = 0; i < s.length(); i++ )
+        {
+            Node* tmp = current->findChild(s[i]);
+            if ( tmp == NULL )
+                return false;
+            current = tmp;
+        }
+
+        return countWordsFromNode(current);
+    }
+
+    return false;
+}
+
+
+
+int main(){
+    int n;
+    cin >> n;
+    Trie * t = new Trie();
+    for(int a0 = 0; a0 < n; a0++){
+        string op;
+        string contact;
+        cin >> op >> contact;
+        if (op == "add") {
+            t->addWord(contact);
+        }
+        if (op == "find") {
+            cout << t->searchWord(contact) << endl;
+        }
+    }
+    return 0;
+}
